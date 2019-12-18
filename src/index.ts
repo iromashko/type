2b1d1603ac914b1d5988761ed5481cb76c7a6146
iromashko <iromashko@me.com>
2019-12-18 08:57:12 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { User } from "./model/User";
+
+const user = new User({ name: "testname", age: 55 });
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("trigger", () => {});
+
+console.log(user);
